15c50fa45ffe23b3c8a804470b5394560ecc8a43
philipgabrielromano <104011942+philipgabrielromano@users.noreply.github.com>
2025-05-13 11:16:00 -0400

    Update server.js -- port fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const API_BASE = 'https://app.uprightlabs.com/api/reports';
 
 app.use(express.static('public'));
